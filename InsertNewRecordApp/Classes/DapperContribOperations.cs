1b58d5d9ca44c03953e7fded31fdfb78578138ea
Karen Payne <paynekaren@comcast.net>
2023-10-31 15:13:45 -0700

    Started code for Dapper.Contrib

---
+++ InsertNewRecordApp/Classes/DapperContribOperations.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using InsertNewRecordApp.Handlers;
+using Microsoft.Data.SqlClient;
+using Dapper.Contrib.Extensions;
+using InsertNewRecordApp.Models;
+
+namespace InsertNewRecordApp.Classes;
+/// <summary>
+/// This class is for https://github.com/DapperLib/Dapper.Contrib
+/// TODO Add CRUD
+/// </summary>
+public partial class DataOperations
+{
+    /// <summary>
+    /// Get all <see cref="Customer"/> in database table
+    /// </summary>
+    /// <returns></returns>
+    public static async Task<List<Customer>> GetAllContrib()
+    {
+
+        SqlMapper.AddTypeHandler(new DapperSqlDateOnlyTypeHandler());
+
+        await using SqlConnection cn = new(ConnectionString());
+
+        return (List<Customer>)await cn.GetAllAsync<Customer>();
+    }
+}
---
+++ InsertNewRecordApp/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using Dapper.Contrib.Extensions;
+
+namespace InsertNewRecordApp.Models;
+
+[Table("Customer")]
+public class Customer
+{
+    [Key]
+    public int Id { get; set; }
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public bool Active { get; set; }
+    public override string ToString() => $"{FirstName} {LastName}";
+
+}
