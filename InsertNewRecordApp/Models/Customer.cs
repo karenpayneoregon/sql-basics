da0a6fda6ca75c7bad4a152605bc5d79c319072b
Karen Payne <paynekaren@comcast.net>
2023-10-31 15:15:49 -0700

    remove line

--- InsertNewRecordApp/Models/Customer.cs
+++ InsertNewRecordApp/Models/Customer.cs
@@ -7,7 +7,6 @@ public class Customer
 {
     [Key]
     public int Id { get; set; }
-
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public bool Active { get; set; }
