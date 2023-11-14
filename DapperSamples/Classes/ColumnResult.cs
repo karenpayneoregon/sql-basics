05d7ce690e69bccd954c603b36f280e079f4bd09
Karen Payne <paynekaren@comcast.net>
2023-11-14 05:50:16 -0800

    :curly_loop: moved model to new file

--- DapperSamples/Classes/ColumnResult.cs
+++ DapperSamples/Classes/ColumnResult.cs
@@ -4,13 +4,3 @@ public class ColumnResult
     public string Value { get; set; }
     public int Length { get; set; }
 }
-
-public class Columns
-{
-    public string TableName { get; set; }
-    public string ColumnName { get; set; }
-    public int MaxLength { get; set; }
-    public string DataType { get; set; }
-    public int DataWidth { get; set; }
-
-}
---
+++ DapperSamples/Classes/Columns.cs
@@ -0,0 +1,11 @@
+﻿namespace DapperSamples.Classes;
+
+public class Columns
+{
+    public string TableName { get; set; }
+    public string ColumnName { get; set; }
+    public int MaxLength { get; set; }
+    public string DataType { get; set; }
+    public int DataWidth { get; set; }
+
+}
