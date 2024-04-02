eeb8388265dbeb190a275c2ce23302b2fdc6f61e
Karen Payne <paynekaren@comcast.net>
2024-04-02 09:17:56 -0700

    tostring

--- DapperForAccessDatabaseMdb/Classes/Person.cs
+++ DapperForAccessDatabaseMdb/Classes/Person.cs
@@ -8,5 +8,4 @@ public class Person
     public DateTime BirthDate { get; set; }
     public bool Active { get; set; }
     public int SomeNumber { get; set; }
-    public override string ToString() => $"{Id,-4}{FirstName,-5}{LastName,-15}{BirthDate,-15:d}{Active,-4}{SomeNumber}";
 }
