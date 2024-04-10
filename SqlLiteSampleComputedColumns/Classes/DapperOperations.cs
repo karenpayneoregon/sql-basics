ebf5d49a81b72a80e627e31751e1b734fe7fa30a
Karen Payne <paynekaren@comcast.net>
2024-04-10 06:57:56 -0700

    dapper code

---
+++ SqlLiteSampleComputedColumns/Classes/DapperOperations.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using System.Data;
+using System.Data.SQLite;
+
+namespace SqlLiteSampleComputedColumns.Classes;
+internal class DapperOperations
+{
+    private static string ConnectionString()
+        => "Data Source=sample1.db";
+    public static List<Person> GetPeople()
+    {
+        var cn = new SQLiteConnection(ConnectionString());
+
+        return cn.Query<Person>(
+                """
+                SELECT Id,
+                       FirstName,
+                       LastName,
+                       FullName
+                FROM ComputedSample
+                """)
+            .AsList();
+    }
+}
+
+public class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string FullName { get; set; }
+}
--- SqlLiteSampleComputedColumns/Program.cs
+++ SqlLiteSampleComputedColumns/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace SqlLiteSampleComputedColumns;
+﻿using SqlLiteSampleComputedColumns.Classes;
+
+namespace SqlLiteSampleComputedColumns;
 
 internal partial class Program
 {
     static void Main(string[] args)
     {
-        AnsiConsole.MarkupLine("[yellow]Hello[/]");
+        var people = DapperOperations.GetPeople();
+        foreach (var person in people)
+        {
+            Console.WriteLine($"{person.Id,-5}{person.FullName}");
+        }
         Console.ReadLine();
     }
 }
