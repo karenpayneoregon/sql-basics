c3dfa40189421ad654ba7b8a4f4ff39ec58627c4
Karen Payne <paynekaren@comcast.net>
2024-04-10 06:47:31 -0700

    removed pragma

--- SqlLiteSampleComputedColumns/Data/COEDDotnetLandVS2022WritingSqlTipsSqlLiteSampleComputedColumnsSample1dbContext.cs
+++ SqlLiteSampleComputedColumns/Data/COEDDotnetLandVS2022WritingSqlTipsSqlLiteSampleComputedColumnsSample1dbContext.cs
@@ -21,7 +21,6 @@ public partial class COEDDotnetLandVS2022WritingSqlTipsSqlLiteSampleComputedColu
     public virtual DbSet<Person> Person { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlite("Data Source=Sample1.db");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
