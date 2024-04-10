3faaef04235b628f8cc10d0c8700a11cb08f83c2
Karen Payne <paynekaren@comcast.net>
2024-04-10 06:46:48 -0700

    connection string

--- SqlLiteSampleComputedColumns/Data/COEDDotnetLandVS2022WritingSqlTipsSqlLiteSampleComputedColumnsSample1dbContext.cs
+++ SqlLiteSampleComputedColumns/Data/COEDDotnetLandVS2022WritingSqlTipsSqlLiteSampleComputedColumnsSample1dbContext.cs
@@ -22,7 +22,7 @@ public partial class COEDDotnetLandVS2022WritingSqlTipsSqlLiteSampleComputedColu
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlite("Data Source=C:\\OED\\DotnetLand\\VS2022\\WritingSqlTips\\SqlLiteSampleComputedColumns\\Sample1.db");
+        => optionsBuilder.UseSqlite("Data Source=SqlLiteSampleComputedColumns\\Sample1.db");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
