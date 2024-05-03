fed3447cee54146bb21fbe639ee13a236e0b8564
Karen Payne <paynekaren@comcast.net>
2024-05-03 04:45:16 -0700

    :curly_loop:

--- SqlLiteInsertNulls/Program.cs
+++ SqlLiteInsertNulls/Program.cs
@@ -6,8 +6,9 @@ internal partial class Program
 {
     static void Main(string[] args)
     {
-        var result = DapperOperations.InsertData(MockedData.GetMockedData());
-        AnsiConsole.MarkupLine("[yellow]Hello[/]");
+        var count = DapperOperations.InsertData(MockedData.GetMockedData());
+        
+        AnsiConsole.MarkupLine($"[yellow]Records added[/] {count}");
         Console.ReadLine();
     }
 }
