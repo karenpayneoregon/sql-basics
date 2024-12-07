0ac03294e6d837b3510568659ca486b43c549b57
Karen Payne <paynekaren@comcast.net>
2024-12-07 05:12:16 -0800

    Refactor School.cs and split classes into new files
    Removed unused using directives and extra line breaks in School.cs.
    Moved Term and OpeningHours classes to separate files Term.cs and
    OpeningHours.cs, respectively. Added necessary using directives
    and namespace declarations in the new files.

--- SqlServerDateOnlyTimeOnlySampleApp/Models/School.cs
+++ SqlServerDateOnlyTimeOnlySampleApp/Models/School.cs
@@ -1,11 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SqlServerDateOnlyTimeOnlySampleApp.Models;
+﻿namespace SqlServerDateOnlyTimeOnlySampleApp.Models;
 public class School
 {
     public int Id { get; set; }
@@ -15,28 +8,4 @@ public class School
     public TimeSpan LegacyTime { get; set; }
     public List<Term> Terms { get; } = new();
     public List<OpeningHours> OpeningHours { get; } = new();
-}
-
-public class Term
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = null!;
-    public DateOnly FirstDay { get; set; }
-    public DateOnly LastDay { get; set; }
-    public School School { get; set; } = null!;
-}
-
-[Owned]
-public class OpeningHours
-{
-    public OpeningHours(DayOfWeek dayOfWeek, TimeOnly? opensAt, TimeOnly? closesAt)
-    {
-        DayOfWeek = dayOfWeek;
-        OpensAt = opensAt;
-        ClosesAt = closesAt;
-    }
-
-    public DayOfWeek DayOfWeek { get; private set; }
-    public TimeOnly? OpensAt { get; set; }
-    public TimeOnly? ClosesAt { get; set; }
 }
