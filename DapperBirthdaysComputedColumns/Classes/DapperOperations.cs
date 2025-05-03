1b221396e9a2e14a6612cb1d0aa72a49322db5d6
Karen Payne <paynekaren@comcast.net>
2025-05-03 16:56:05 -0700

    Enhance DapperOperations and clean up BirthDays
    Updated the `DapperOperations` class to include a constructor that initializes a `SqlConnection` and registers a custom type handler for `DateOnly`. Added detailed XML documentation for the constructor and `GetBirthdaysAsync` method.
    
    Removed unnecessary `using` directives in `BirthDays.cs` for cleaner code.
    
    by Karen

--- DapperBirthdaysComputedColumns/Classes/DapperOperations.cs
+++ DapperBirthdaysComputedColumns/Classes/DapperOperations.cs
@@ -10,12 +10,38 @@ internal class DapperOperations
 {
     private IDbConnection _cn;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DapperOperations"/> class.
+    /// </summary>
+    /// <remarks>
+    /// This constructor sets up the database connection using the main connection string 
+    /// from <see cref="AppConnections.Instance.MainConnection"/> and registers a custom 
+    /// type handler, <see cref="SqlDateOnlyTypeHandler"/>, for handling <see cref="DateOnly"/> types.
+    /// </remarks>
+
     public DapperOperations()
     {
         _cn = new SqlConnection(AppConnections.Instance.MainConnection);
         SqlMapper.AddTypeHandler(new SqlDateOnlyTypeHandler());
     }
 
+    /// <summary>
+    /// Retrieves a list of birthdays from the database asynchronously.
+    /// </summary>
+    /// <remarks>
+    /// This method executes a SQL query defined in <see cref="SqlStatements.GetBirthdays"/> 
+    /// to fetch data from the database and maps the results to a list of <see cref="BirthDays"/> objects.
+    /// The data includes a computed column, <see cref="BirthDays.YearsOld"/>, which represents the age of the individual.
+    /// </remarks>
+    /// <returns>
+    /// A task that represents the asynchronous operation. The task result contains a list of <see cref="BirthDays"/> objects.
+    /// </returns>
+    /// <exception cref="SqlException">
+    /// Thrown when there is an issue executing the SQL query.
+    /// </exception>
+    /// <exception cref="InvalidOperationException">
+    /// Thrown when the database connection is not properly initialized.
+    /// </exception>
     public async Task<List<BirthDays>> GetBirthdaysAsync()
     {
         return (await _cn.QueryAsync<BirthDays>(SqlStatements.GetBirthdays)).AsList();
--- DapperBirthdaysComputedColumns/Models/BirthDays.cs
+++ DapperBirthdaysComputedColumns/Models/BirthDays.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DapperBirthdaysComputedColumns.Models;
+﻿namespace DapperBirthdaysComputedColumns.Models;
 internal class BirthDays
 {
     public int Id { get; set; }
