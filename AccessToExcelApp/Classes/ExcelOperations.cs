9a1c750a972b5c5c23f1e052baf3f973df2a198f
Karen Payne <paynekaren@comcast.net>
2025-10-21 11:01:31 -0700

    clean-up

--- AccessToExcelApp/Classes/ExcelOperations.cs
+++ AccessToExcelApp/Classes/ExcelOperations.cs
@@ -1,11 +1,7 @@
 ﻿using DocumentFormat.OpenXml.Spreadsheet;
 using SpreadsheetLight;
-using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 // ReSharper disable CollectionNeverQueried.Local
 
 namespace AccessToExcelApp.Classes;
