7ed0bed475eae4a1d7f3e1c04ef2cf3299eb4df1
Karen Payne <paynekaren@comcast.net>
2026-02-17 12:44:00 -0800

    Move CallingExamplesForAddingCustomer to new file
    Refactored CallingExamplesForAddingCustomer from MethodExamples.cs
    to its own file, CallingExamplesForAddingCustomer.cs. Updated
    namespace and added necessary using directives. Changed call to
    SuccessPill to use SpectreConsoleHelpers. No changes to method
    logic.
    
    by Karen

---
+++ NorthWindSqlLiteApp1/Classes/MethodsExamples/CallingExamplesForAddingCustomer.cs
@@ -0,0 +1,56 @@
+﻿using NorthWindSqlLiteApp1.Classes.Core;
+using Spectre.Console;
+
+namespace NorthWindSqlLiteApp1.Classes.MethodsExamples;
+
+public class CallingExamplesForAddingCustomer
+{
+    public static void AddingSingleCustomer()
+    {
+ 
+        // nothing returned
+        MethodExamples.AddCustomer1();
+
+        bool successfully = MethodExamples.AddCustomer2();
+        if (successfully)
+        {
+            SpectreConsoleHelpers.SuccessPill(Justify.Left, "Customer added successfully using AddCustomer2.");
+        }
+        else
+        {
+            // Failed to add customer
+        }
+
+        
+        {
+
+            var (success, identifier) = MethodExamples.AddCustomer3();
+            if (success)
+            {
+                // Use the identifier as needed
+            } else    
+            {
+                // Failed to add customer
+            }
+
+        }
+
+        {
+            var (success, identifier, exception) = MethodExamples.AddCustomer4();
+            if (success)
+            {
+                // Customer added with identifier
+            }
+            else
+            {
+                // Failed to add customer 
+                if (exception is not null)
+                {
+                    // see log for details
+                }
+            }
+
+        }
+
+    }
+}
